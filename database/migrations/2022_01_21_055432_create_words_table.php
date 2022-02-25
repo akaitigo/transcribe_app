48307ddb185755dd32dc9c84cccf56b864c10f7a
akaitigo <ryusei1116nakayama@outlook.com>
2022-02-25 23:46:25 +0900

    [中山]wordsテーブルにカラムを追加

--- database/migrations/2022_01_21_055432_create_words_table.php
+++ database/migrations/2022_01_21_055432_create_words_table.php
@@ -16,6 +16,7 @@ class CreateWordsTable extends Migration
         Schema::create('words', function (Blueprint $table) {
             $table->id();
             $table->string('name',255)->comment('単語名');
+            $table->timestamps();
         });
     }
 
